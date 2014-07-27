37c278e4a83be5f16c21ff6b17b3178ee3cebece
meads58 <meads109@gmail.com>
2014-07-27 08:08:40 +0100

    deleted calc.rb and moved to chapter1. Chapter2 folder created

--- chapter3/examples3.rb
+++ chapter3/examples3.rb
@@ -1,1 +1,14 @@
-puts 'test'
+#chapter 3 exercises, Strings
+
+puts 'I like' + ' stuff'
+puts '4 words: ' * 4
+
+puts 2 * 5
+puts '2' * 5 #writes out the string '2' five times.
+
+#escaping text to all you to put ' and " into a string. Use \ to do this
+puts 'You\'re swell!!' #the \ has escaped the '
+puts 'backslash at the end of a string: \\'#this escapes the \ if you only had 1 \ then the ' would escape making 
+# the text invalid.
+puts 'up\\down'
+puts 'up\down' #note that \d doesn't do anything 

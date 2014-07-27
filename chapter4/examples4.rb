8e3de00413e16c2e19b3f837b39108eb81a1a5aa
meads58 <meads109@gmail.com>
2014-07-27 08:25:29 +0100

    chapter 4 variables done

---
+++ chapter4/examples4.rb
@@ -0,0 +1,15 @@
+#chapter 4 variables and assignments examples
+
+my_string = '...some stuff is written'
+
+puts my_string
+puts my_string * 2 #prints the variable twice
+
+var1 = 8
+var2 = var1
+puts var1
+puts var2
+puts ''
+var1 = 'eights'
+puts var1
+puts var2

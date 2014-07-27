839f8dc4607422667ad5061ffe89fc46c9eee844
meads58 <meads109@gmail.com>
2014-07-27 08:14:01 +0100

    updated index

--- calc.rb
+++
@@ -1,5 +0,0 @@
-puts 1+2
-puts 'there are ' + (24 * 365).to_s + ' hours in a year' #to_s converts an integer to a string
-puts (60 * 24 * 365 * 10).to_s + ' minutes in a decade'
-puts 'My age in seconds is: ' + (60*60*24*365*34).to_s
-puts 'The author is ' + ((((1160000000/60)/60)/24)/365).to_s + ' years old'
